064e61208c14554c5d19c18941d8db0b6c3b5248
David Alejandro Alvarado Villa <david.alvarado.v@hotmail.com>
2017-04-19 12:47:19 -0500

    Clase Producto

---
+++ InventarioListasSimples/InventarioListasSimples/Producto.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventarioListasSimples
+{
+    class Producto
+    {
+        private int _codigo;
+        public int codigo
+        {
+            set { _codigo = value; }
+            get { return _codigo; }
+        }
+
+        private string _nombre;
+        public void setNombre(string nombre)
+        {
+            _nombre = nombre;
+        }
+
+        private int _cantidad;
+        public int catidad
+        {
+            set { _cantidad = value; }
+            get { return _cantidad; }
+        }
+
+        private int _costo;
+        public int costo
+        {
+            set { _costo = value; }
+            get { return _costo; }
+        }
+
+        public Producto()
+        {
+            _codigo = 0;
+            _nombre = "";
+            _cantidad = 0;
+            _costo = 0;
+        }
+
+        public override string ToString()
+        {
+            return "Código: " + _codigo.ToString() + " Nombre " + _nombre.ToString() + " Cantidad " + _cantidad.ToString() + " Costo: " + _codigo.ToString();
+        }
+    }
+}
