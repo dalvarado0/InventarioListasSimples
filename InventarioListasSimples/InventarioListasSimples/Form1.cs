3c31d064da392c7d5ad94a0458912a3ad66da1de
David Alejandro Alvarado Villa <david.alvarado.v@hotmail.com>
2017-04-23 12:32:23 -0500

    Modificacion en el formulario,  el boton de reporte funciona bien pero el de agragar da un error de recursividad o bucle infinito cuando quiero guardar un segundo producto.

--- InventarioListasSimples/InventarioListasSimples/Form1.cs
+++ InventarioListasSimples/InventarioListasSimples/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace InventarioListasSimples
@@ -21,20 +14,15 @@ namespace InventarioListasSimples
 
         private void btnAgregar_Click(object sender, EventArgs e)
         {
+            
             Producto item = new Producto();
             item.codigo = Convert.ToInt32(txbCodigo.Text);
             item.setNombre(txbNombre.Text);
             item.catidad = Convert.ToInt32(txbCandidad.Text);
             item.costo = Convert.ToInt32(txbPrecio.Text);
-            
-            if (almacen.agregarItem(item))
-            {
-                MessageBox.Show("Producto agregado");
-            }
-            else
-            {
-                MessageBox.Show("Producto no agregador");
-            }
+
+            almacen.agregar(item);
+
         }
 
         private void btnEliminar_Click(object sender, EventArgs e)
@@ -44,21 +32,13 @@ namespace InventarioListasSimples
             item.setNombre(txbNombre.Text);
             item.catidad = Convert.ToInt32(txbCandidad.Text);
             item.costo = Convert.ToInt32(txbPrecio.Text);
-
-            if (almacen.eliminarItem(item))
-            {
-                MessageBox.Show("Producto eliminado");
-            }
-            else
-            {
-                MessageBox.Show("Producto no eliminado");
-            }
+          
         }
 
         private void btnReporte_Click(object sender, EventArgs e)
         {
-            txbReporte.Text = almacen.reporteItem();
-            MessageBox.Show("dasd");
+            txbReporte.Text = "";
+            txbReporte.Text = almacen.reporte();
         }
 
         private void btnInsertar_Click(object sender, EventArgs e)
@@ -68,7 +48,7 @@ namespace InventarioListasSimples
             item.setNombre(txbNombre.Text);
             item.catidad = Convert.ToInt32(txbCandidad.Text);
             item.costo = Convert.ToInt32(txbPrecio.Text);
-            almacen.insertarItem(item, Convert.ToInt32(txbPosicion.Text));
+            //almacen.insertarItem(item, Convert.ToInt32(txbPosicion.Text));
         }
 
         private void frmPrincipal_Load(object sender, EventArgs e)
@@ -78,17 +58,20 @@ namespace InventarioListasSimples
 
         private void btnBuscar_Click(object sender, EventArgs e)
         {
-            Producto item;
-            //item.codigo = Convert.ToInt32(txbBuscar.Text);
-            item = almacen.buscarProducto(Convert.ToInt32(txbBuscar.Text));
-            if (item == null)
-                txbReporte.Text = "No exite el producto";
-            else
-            {
-                txbReporte.Text = item.ToString();
-            }
+            /*
+           Producto item;
+
+           //item.codigo = Convert.ToInt32(txbBuscar.Text);
+           item = almacen.buscarProducto(Convert.ToInt32(txbBuscar.Text));
+           if (item == null)
+               txbReporte.Text = "No exite el producto";
+           else
+           {
+               txbReporte.Text = item.ToString();
+           }
+           */
         }
 
-        
+
     }
 }
