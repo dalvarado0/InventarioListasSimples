b0bbe6e1c50657f473f0d8956ecdd3f9369a0935
David Alejandro Alvarado Villa <david.alvarado.v@hotmail.com>
2017-04-26 22:26:53 -0500

    Metodos de insertar, eliminar, reporte y buscar

--- InventarioListasSimples/InventarioListasSimples/Inventario.cs
+++ InventarioListasSimples/InventarioListasSimples/Inventario.cs
@@ -8,39 +8,94 @@ namespace InventarioListasSimples
 {
     class Inventario
     {
-        Producto inicio;
+        private Producto inicio;
 
         public Inventario()
         {
             //inicio = new Producto();
         }
 
+        //public void agregar(Producto nuevo)
+        //{
+        //    if (inicio == null)
+        //        inicio = nuevo;
+        //    else
+        //    {
+        //        Producto temp = inicio;
+        //        while (temp.siguiente != null)
+        //            temp = temp.siguiente;
+        //        temp.siguiente = nuevo;
+
+        //    }
+        //}
+
         public void agregar(Producto nuevo)
         {
-            if(inicio == null)
+            if (inicio == null)
                 inicio = nuevo;
             else
+                agregar(inicio, nuevo);
+        }
+
+        private void agregar(Producto ultimo, Producto nuevo)
+        {
+            if (ultimo.siguiente == null)
+                ultimo.siguiente = nuevo;
+            else
+                agregar(ultimo.siguiente, nuevo);
+        }
+
+        public void insertar(Producto nuevo, int posicion)
+        {
+            Producto temp = inicio;
+            int cont = 1;
+            while(temp.siguiente != null || cont < posicion)
             {
-                Producto temp = inicio;
-                while (temp.siguiente != null)
-                    temp = temp.siguiente;
-                temp.siguiente = nuevo;
-                
+                cont++;
+                if(cont == posicion)
+                {
+                    nuevo.siguiente = temp.siguiente;
+                    temp.siguiente = nuevo;
+                }
             }
+        }
+
+        public void eliminar(int codigo)
+        {
+            Producto temp = inicio;
+            while(temp.siguiente != null)
+            {
+                if(temp.siguiente.codigo == codigo)
+                    temp.siguiente = temp.siguiente.siguiente;
+                temp = temp.siguiente;
+            }
+        }
+
+        public Producto buscar(int codigo)
+        {
+            Producto item = null, temp = inicio;
+            while(temp.siguiente != null)
+            {
+                if (temp.siguiente.codigo == codigo)
+                    item = temp.siguiente;
+                temp = temp.siguiente;
+            }
+            return item;
         }
 
         public string reporte()
         {
             string datos = "";
 
-            Producto t = inicio;
-            while(t != null)
+            Producto temp = inicio;
+            while (temp != null)
             {
-                datos += t.ToString() + Environment.NewLine;
-                t = t.siguiente;
+                datos += temp.ToString() + Environment.NewLine;
+                temp = temp.siguiente;
             }
 
             return datos;
+        }
         }
     }
 }
